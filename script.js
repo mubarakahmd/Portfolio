0e6d0e2791efabb0ba4e29cf23706e5fe08d29e8
mubarakahmd <mubaraktk952@gmail.com>
2024-07-17 16:07:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+var typed= new Typed(".texttt",{
+  strings:[ "Frontend Developer","Graphic Designer"],
+  typeSpeed:100,
+  backSpeed:100,
+  backDelay:1000,
+  loop:true
+});
